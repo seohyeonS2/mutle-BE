074e4494e2e89314ac9f08ebad6ac943b21d71b4
parkminsuyang <shyeon16@ewha.ac.kr>
2026-01-27 22:05:46 +0900

    Initial commit

---
+++ src/main/java/com/mutle/mutle/MutleApplication.java
@@ -0,0 +1,13 @@
+package com.mutle.mutle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutle/mutle/MutleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutle.mutle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
