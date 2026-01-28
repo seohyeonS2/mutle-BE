0032b1ff3940171a4b177c87da7dd1b0e83c3372
jaeeun <mydlbxiu@ewhain.net>
2026-01-28 19:47:50 +0900

    [Feat] Reaction, TodayQuest 엔티티 설계

--- src/main/java/com/mutle/mutle/entity/Reaction.java
+++ src/main/java/com/mutle/mutle/entity/Reaction.java
@@ -1,4 +1,39 @@
 package com.mutle.mutle.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "reactions")
+@Builder
 public class Reaction {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "reaction_id")
+    private Long reactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "bottle_id", nullable = false)
+    private Bottle bottle;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private User reactor;
+
+    @Column(name = "reaction_created_at", nullable = false, updatable = false)
+    private Timestamp reationCreatedAt = Timestamp.valueOf(LocalDateTime.now());
+
+    @UpdateTimestamp
+    @Column(name = "reaction_updated_at", nullable = false)
+    private Timestamp reactionUpdatedAt = Timestamp.valueOf(LocalDateTime.now());
 }
--- src/main/java/com/mutle/mutle/entity/TodayQuest.java
+++ src/main/java/com/mutle/mutle/entity/TodayQuest.java
@@ -1,7 +1,39 @@
 package com.mutle.mutle.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
+@Table(name = "today_quests")
+@Builder
 public class TodayQuest {
+    @Id
+    @GeneratedValue
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(name = "question_text", nullable = false, length = 255)
+    private String questionText;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Column(name = "question_created_at", nullable = false, updatable = false)
+    private Timestamp questionCreatedAt = Timestamp.valueOf(LocalDateTime.now());
+
+    @UpdateTimestamp
+    @Column(name = "question_updated_at", nullable = false)
+    private Timestamp questionUpdatedAt = Timestamp.valueOf(LocalDateTime.now());
+
+
 }
